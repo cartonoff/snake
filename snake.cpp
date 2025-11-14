a5c5dd45bfb35965878cd9ec3265bb42b8591d14
cartonoff <nikolaikoaleksandr@gmail.com>
2025-11-14 22:44:42 +0300

    commit

---
+++ snake.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int Xsize, Ysize;
+int Xc, Yc;
+
+
+
+void Draw(){
+    for (int y = 0; y < Ysize+2; y++)
+    {
+        for (int x = 0; x < Xsize; x++)
+        {
+            
+        }
+        
+    }
+    
+}
+
+
+int main()
+{
+    cin>>Xsize>>Ysize;
+
+}
